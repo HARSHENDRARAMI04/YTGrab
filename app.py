c039018d53fc133a8d44865a88410511c8d29824
Harshendra Rami <150004492+HARSHENDRARAMI04@users.noreply.github.com>
2024-06-06 15:46:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-import re
-import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from googleapiclient.discovery import build
 from pytube import YouTube
 from dotenv import load_dotenv
 from google.auth.exceptions import DefaultCredentialsError
 from pathlib import Path
-
-
-
-
-
-
-
+import re
+import os
 
 app = Flask(__name__)
 
@@ -88,23 +81,18 @@ def get_video_urls(playlist_id):
     return video_urls
 
 def download_videos(playlist_id):
-    dir = make_folder(playlist_id)
+    home = str(Path.home())
+    downloads_path = os.path.join(home, "Downloads")
+    
+    if not os.path.exists(downloads_path):
+        os.makedirs(downloads_path)
+    
     urls = get_video_urls(playlist_id)
+    
     for each in urls:
         yt = YouTube(each)
         stream = yt.streams.get_highest_resolution()
-        stream.download(output_path=dir)
-
-def make_folder(playlist_id):
-    home = str(Path.home())
-    downloads_path = os.path.join(home, "Downloads")
-    if not os.path.exists(downloads_path):
-        os.makedirs(downloads_path)
-    name = get_playlist_name(playlist_id)
-    folder_path = os.path.join(downloads_path, name)
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-    return folder_path
+        stream.download(output_path=downloads_path)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -127,7 +115,7 @@ def index():
 def download():
     playlist_id = request.form['playlist_id']
     download_videos(playlist_id)
-    return redirect(url_for('index'))
+    return jsonify({"status": "success"})
 
 @app.route('/terms')
 def terms():
